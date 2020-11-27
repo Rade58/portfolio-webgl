97c069b4f01895d2a93689df8e144d57507b6bbe
rade <bajic.rade2@gmail.com>
2020-11-27 17:30:53 +0100

    :octocat: :space_invader:

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import { FunctionComponent } from "react";
+
+const PageName: FunctionComponent = () => {
+  return (
+    <div>
+      👾
+    </div>
+  );
+};
+
+export default PageName;
