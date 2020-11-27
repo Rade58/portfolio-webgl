7cc8df5cadd8eab4f2761d0b93c8a3163dc35571
rade <bajic.rade2@gmail.com>
2020-11-28 00:59:02 +0100

    :octocat: :space_invader:

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppProps } from "next/app";
+
+import { ThemeProvider } from "theme-ui";
+
+import theme from "../theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
