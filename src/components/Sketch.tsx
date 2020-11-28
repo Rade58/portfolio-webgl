c8d30e6eabe0cbfa685e851dacd251846479956b
rade <bajic.rade2@gmail.com>
2020-11-28 01:09:40 +0100

    :octocat: :space_invader:

--- src/components/Sketch.tsx
+++ src/components/Sketch.tsx
@@ -1,9 +1,9 @@
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 /** @jsxRuntime classic */
 /** @jsx jsx */
-// import { jsx } from "theme-ui";
+
+import { jsx } from "theme-ui";
 import { FunctionComponent, useRef, useEffect } from "react";
-import { css, jsx } from "@emotion/react";
 
 const Sketch: FunctionComponent = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -16,9 +16,9 @@ const Sketch: FunctionComponent = () => {
 
   return (
     <div
-      css={css`
-        border: crimson solid 4px;
-      `}
+      sx={{
+        border: "crimson solid 2px",
+      }}
     >
       <canvas className="canvas" ref={canvasRef}></canvas>
     </div>
