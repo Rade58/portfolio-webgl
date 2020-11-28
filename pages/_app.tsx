e25fb3b8fd667f4a9b448d6716ce0548a96831a4
rade <bajic.rade2@gmail.com>
2020-11-28 16:24:36 +0100

    :octocat: :space_invader:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Global } from "@emotion/react";
 import { AppProps } from "next/app";
 
 import { ThemeProvider } from "theme-ui";
@@ -10,6 +11,21 @@ console.log({ blah: JSON.stringify(theme.styles.h1) });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Global
+        styles={{
+          body: {
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+            margin: 0,
+          },
+          html: {},
+        }}
+      />
+
       <Component {...pageProps} />
     </ThemeProvider>
   );
