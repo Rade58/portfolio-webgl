3d8e1435cd1db20930954218db77bdf47842ba74
rade <bajic.rade2@gmail.com>
2020-11-28 18:52:44 +0100

    :octocat: :space_invader:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "theme-ui";
 
 import theme from "../theme";
 
-console.log({ blah: JSON.stringify(theme.styles.h1) });
+/* console.log({ blah: JSON.stringify(theme.styles.h1) }); */
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
