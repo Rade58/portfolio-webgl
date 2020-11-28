99188cfc3bea49908c72825127977fed83a42bb3
rade <bajic.rade2@gmail.com>
2020-11-28 15:53:00 +0100

    :octocat: :space_invader:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import { FunctionComponent, Fragment } from "react";
-import Sketch from "../src/components/Sketch";
+import Other from "../src/components/Other";
+import Sketch from "../src/components/sketch/Sketch";
 
 const Index: FunctionComponent = () => {
   return (
     <Fragment>
       Welcome
       <Sketch />
+      <Other />
     </Fragment>
   );
 };
---
+++ src/components/Other.tsx
@@ -0,0 +1,17 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+import { FunctionComponent } from "react";
+
+const Other: FunctionComponent = () => {
+  return (
+    <div
+      css={css`
+        border: crimson solid 1px;
+      `}
+    ></div>
+  );
+};
+
+export default Other;
