b75259ba18805b00cc60d0caad8db45dfd637ecd
rade <bajic.rade2@gmail.com>
2020-12-12 22:09:14 +0100

    :octocat: :space_invader:

---
+++ pages/test.tsx
@@ -0,0 +1,20 @@
+/* eslint react/react-in-jsx-scope: 0 */
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+import { FunctionComponent } from "react";
+import { sketchService, EE } from "../state_machines/sketch_state_machine";
+
+const TestPage: FunctionComponent = () => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          sketchService.send({ type: EE.TOGGLE });
+        }}
+      >
+        toggle
+      </button>
+    </div>
+  );
+};
+
+export default TestPage;
