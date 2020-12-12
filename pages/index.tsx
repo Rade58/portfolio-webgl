d9a3ed938fc2ec14ec04a7a5395f2d50ec5c9aaf
rade <bajic.rade2@gmail.com>
2020-12-12 20:51:45 +0100

    :octocat: :space_invader:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
 import { FunctionComponent, Fragment } from "react";
-import Other from "../src/components/Other";
-import OtherSec from "../src/components/OtherSec";
-import Sketch from "../src/components/sketch/Sketch";
+import Other from "../components/Other";
+import OtherSec from "../components/OtherSec";
+import Sketch from "../components/sketch/Sketch";
 
 const Index: FunctionComponent = () => {
   return (
