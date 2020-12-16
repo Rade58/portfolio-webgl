cee5f384585f57489619d6a0d6502c04cf954cbb
rade <bajic.rade2@gmail.com>
2020-12-16 04:47:13 +0100

    :octocat: :space_invader:

---
+++ sketch/glsl_stuff/icosaItemVertes.ts
@@ -0,0 +1,25 @@
+const glsl = require("glslify");
+
+export default glsl(/* glsl */ `
+
+    varying vec3 vPosition;
+    varying vec2 vUv;
+
+
+    void main(){
+
+      vPosition = position;
+      vUv = uv;
+      /*
+      vec3 transformed = position.xyz;
+
+      transformed.xz -= 1.0;
+      transformed.yz -= 0.2;
+      */
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+    }
+
+
+  `);
