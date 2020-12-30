34802cad57ee72d154d7b4ab8f3ba0b61ca5101e
Rade58 <bajic.rade2@gmail.com>
2020-12-30 03:42:52 +0100

    :octocat: :space_invader:

--- state_machines/app_machine.ts
+++ state_machines/app_machine.ts
@@ -1,6 +1,10 @@
 import { createMachine, assign, interpret } from "xstate";
 
 import { CssClassesEnum } from "../sketch/ui/user_interface";
+import {
+  fse as animeFse,
+  MAJOR_FINITE_STATES_ARRAY,
+} from "../sketch/machine/anim_state_machine";
 
 enum fse {
   animation_active = "animation_active",
@@ -10,4 +14,9 @@ enum fse {
 export enum EE {
   CLICK_BACK = "CLICK_BACK",
   CLICK_FORTH = "CLICK_FORTH",
+}
+
+interface ContextI {
+  currentFiniteStateAnimeMachine: animeFse | undefined;
+  currentMajorState: typeof MAJOR_FINITE_STATES_ARRAY[number] | undefined;
 }
