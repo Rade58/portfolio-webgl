f9da9339b5b1a0ecdc1c1cc62af1f1ba5aea77e7
Rade58 <bajic.rade2@gmail.com>
2020-12-31 03:29:52 +0100

    :octocat: :space_invader:

--- context_n_reducers/app_context.ts
+++ context_n_reducers/app_context.ts
@@ -12,15 +12,36 @@ export enum REDUCER_ACTION_TYPES {
   APP_MACINE_LOADED = "APP_MACHINE_LOADED",
 }
 
-export interface ReducerStateI {
+export interface ReducedStateI {
   app_machine_loaded: boolean;
 }
 
 // ------------------------------------------
 
 export const appReducer: Reducer<
-  ReducerStateI,
+  ReducedStateI,
   { type: REDUCER_ACTION_TYPES; payload: any }
 > = (state, action) => {
   return state;
 };
+
+//  -------
+
+export const defaultState = {
+  reducedState: {
+    app_machine_loaded: false,
+  },
+};
+
+export interface ContextStateI {
+  reducedState: ReducedStateI;
+}
+
+// -----------------
+
+export const appContext: Context<ContextStateI> = createContext(defaultState);
+
+const { Consumer, Provider } = appContext;
+
+export const useContextualState_$ = { appContext, REDUCER_ACTION_TYPES };
+export const createContextualState_$ = { Provider, appReducer, defaultState };
