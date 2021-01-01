887762846014ea041db9da110a2ef1fe9ba2067e
Rade58 <bajic.rade2@gmail.com>
2021-01-01 23:00:16 +0100

    :octocat: :space_invader:

--- components/ControlAnim.tsx
+++ components/ControlAnim.tsx
@@ -4,21 +4,13 @@
 import { jsx } from "theme-ui";
 import { FunctionComponent, useContext } from "react";
 import { css } from "@emotion/core";
-import styled from "@emotion/styled";
 
 // import { animfse } from "../state_machines/app_machine";
 import { EE } from "../state_machines/app_machine";
 
-import { appContext } from "../context_n_reducers/app_context";
+import BackNForth from "./svgs/BackNForth";
 
 const ControlAnim: FunctionComponent = () => {
-  const { reducedState } = useContext(appContext);
-  const { appService } = reducedState;
-
-  if (!appService) {
-    return null;
-  }
-
   return (
     <section
       className="anim_control"
@@ -35,69 +27,7 @@ const ControlAnim: FunctionComponent = () => {
       <section>8</section>
       <section>8</section>
       {/*  */}
-      {appService && (
-        <svg
-          tabIndex={0}
-          onClick={() => {
-            if (appService) {
-              console.log("click back");
-              appService.send({ type: EE.CLICK_BACK });
-            }
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              if (appService) {
-                appService.send({ type: EE.CLICK_BACK });
-              }
-            }
-          }}
-          /* NO NEED FOR px ON width AND height */
-          width="200"
-          height="120"
-          aria-labelledby="go_back"
-          id="svg"
-          role="button" /*"presentation"*/ /* or role="imge"*/
-          lang="en"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 380 210"
-        >
-          <title id="go_back">Your Title Goes here</title>
-          <rect width="200" height="180" x="8" y="8" fill="crimson" />
-        </svg>
-      )}
-
-      {appService && (
-        <svg
-          tabIndex={0}
-          onClick={() => {
-            if (appService) {
-              console.log("click forward");
-              appService.send({ type: EE.CLICK_FORTH });
-            }
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              if (appService) {
-                appService.send({ type: EE.CLICK_FORTH });
-              }
-            }
-          }}
-          /* NO NEED FOR px ON width AND height */
-          width="200"
-          height="120"
-          aria-labelledby="go_forward"
-          id="svg"
-          role="button" /*"presentation"*/ /* or role="imge"*/
-          lang="en"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 380 210"
-        >
-          <title id="go_forward">Your Title Goes here</title>
-          <rect width="200" height="180" x="8" y="8" fill="crimson" />
-        </svg>
-      )}
+      <BackNForth />
       {/*  */}
     </section>
   );
---
+++ components/svgs/BackNForth.tsx
@@ -0,0 +1,83 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { FunctionComponent, Fragment, useContext } from "react";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+import { appContext } from "../../context_n_reducers/app_context";
+import { EE } from "../../state_machines/app_machine";
+
+const BackNForth: FunctionComponent = () => {
+  const { reducedState } = useContext(appContext);
+  const { appService } = reducedState;
+
+  if (!appService) {
+    return null;
+  }
+
+  return (
+    <Fragment>
+      <svg
+        tabIndex={0}
+        onClick={() => {
+          if (appService) {
+            console.log("click back");
+            appService.send({ type: EE.CLICK_BACK });
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            if (appService) {
+              appService.send({ type: EE.CLICK_BACK });
+            }
+          }
+        }}
+        /* NO NEED FOR px ON width AND height */
+        width="200"
+        height="120"
+        aria-labelledby="go_back"
+        id="svg"
+        role="button" /*"presentation"*/ /* or role="imge"*/
+        lang="en"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 380 210"
+      >
+        <title id="go_back">Your Title Goes here</title>
+        <rect width="200" height="180" x="8" y="8" fill="crimson" />
+      </svg>
+
+      <svg
+        tabIndex={0}
+        onClick={() => {
+          if (appService) {
+            console.log("click forward");
+            appService.send({ type: EE.CLICK_FORTH });
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            if (appService) {
+              appService.send({ type: EE.CLICK_FORTH });
+            }
+          }
+        }}
+        /* NO NEED FOR px ON width AND height */
+        width="200"
+        height="120"
+        aria-labelledby="go_forward"
+        id="svg"
+        role="button" /*"presentation"*/ /* or role="imge"*/
+        lang="en"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 380 210"
+      >
+        <title id="go_forward">Your Title Goes here</title>
+        <rect width="200" height="180" x="8" y="8" fill="crimson" />
+      </svg>
+    </Fragment>
+  );
+};
+
+export default BackNForth;
