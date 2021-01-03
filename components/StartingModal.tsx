90028c8717de3981b51dd03d7e51c4b6680bc50e
Rade58 <bajic.rade2@gmail.com>
2021-01-03 06:36:13 +0100

    :octocat: :space_invader:

--- components/StartingModal.tsx
+++ components/StartingModal.tsx
@@ -2,9 +2,10 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useRef, useEffect } from "react";
 import { css } from "@emotion/core";
-import styled from "@emotion/styled";
+
+import { TweenMax, Power4 } from "gsap";
 
 import { useService } from "@xstate/react";
 
@@ -13,22 +14,35 @@ import { appService } from "../state_machines/app_machine";
 const StartingModal: FunctionComponent = () => {
   const [state, send] = useService(appService);
   const { canLoadControls } = state.context;
+
+  const modalRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (canLoadControls) {
+      if (modalRef.current) {
+        TweenMax.to(modalRef.current, 0.38, {
+          translateY: "-118%",
+          ease: Power4.easeIn,
+        });
+      }
+    }
+  }, [modalRef, canLoadControls]);
+
   return (
-    !canLoadControls && (
-      <aside
-        css={css`
-          background-color: #207c88;
-          position: fixed;
-          z-index: 8;
-          top: 0;
-          bottom: 0;
-          left: 0;
-          right: 0;
-        `}
-      >
-        spinner
-      </aside>
-    )
+    <aside
+      ref={modalRef}
+      css={css`
+        background-color: #207c88;
+        position: fixed;
+        z-index: 8;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      `}
+    >
+      spinner
+    </aside>
   );
 };
 
