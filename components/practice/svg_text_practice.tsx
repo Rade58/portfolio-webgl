c86bcd9af1fc7c8efaa3c7449093c08260d86455
Rade58 <bajic.rade2@gmail.com>
2021-01-03 02:02:22 +0100

    :octocat: :space_invader:

--- components/practice/svg_text_practice.tsx
+++ components/practice/svg_text_practice.tsx
@@ -11,11 +11,7 @@ const ComponentName: FunctionComponent = () => {
       css={css`
         box-sizing: border-box;
         border: crimson 1px solid;
-        margin: 10px 48px;
-
-        & svg {
-          font-family: Roboto;
-        }
+        margin: 0px 0px;
       `}
     >
       <svg
@@ -29,11 +25,13 @@ const ComponentName: FunctionComponent = () => {
         lang="en"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 380 210"
+        viewBox="0 0 380 60"
       >
         <title id="some_text">Hi it{"'"}s a me</title>
-        <text x="0" y="18" stroke="red" fill="crimson">
-          Roboto
+        <text fill="#a14a7d" stroke="#32727a" wordSpacing="4.8">
+          <tspan fontSize="16" x="24" y="33">
+            Hi, My name is Rade and I build things
+          </tspan>
         </text>
       </svg>
     </div>
