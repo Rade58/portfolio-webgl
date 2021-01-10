5f6c6315665674930f9178c312106c12819dfaf7
Rade58 <bajic.rade2@gmail.com>
2021-01-10 11:49:20 +0100

    :octocat: :space_invader:

--- components/MajorShower.tsx
+++ components/MajorShower.tsx
@@ -6,8 +6,48 @@ import { FunctionComponent } from "react";
 import { css } from "@emotion/core";
 import styled from "@emotion/styled";
 
+import {
+  fse as fsS,
+  MAJOR_FINITE_STATES_ARRAY,
+} from "../sketch/middle_ground/major_states";
+
+import { useService } from "@xstate/react";
+import { storyService } from "../state_machines/story_machine";
+
+const MAJOR_ARR_LENGTH = MAJOR_FINITE_STATES_ARRAY.length;
+
 const MajorShowers: FunctionComponent = () => {
-  return null;
+  const [state, send] = useService(storyService);
+
+  const { major } = state.context;
+
+  if (!major || major === "undefined") {
+    return null;
+  }
+
+  const currIndex: number = MAJOR_FINITE_STATES_ARRAY.indexOf(major);
+
+  const nextIndex: number =
+    currIndex + 1 > MAJOR_ARR_LENGTH - 1 ? 0 : currIndex + 1;
+  const prevIndex: number =
+    currIndex - 1 < 0 ? MAJOR_ARR_LENGTH - 1 : currIndex - 1;
+
+  return (
+    <section
+      className="story"
+      css={css`
+        border: crimson solid 1px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 86vh;
+      `}
+    >
+      <h4>prev: {MAJOR_FINITE_STATES_ARRAY[prevIndex]}</h4>
+      <h1>major: {major}</h1>
+      <h4>next: {MAJOR_FINITE_STATES_ARRAY[nextIndex]}</h4>
+    </section>
+  );
 };
 
 export default MajorShowers;
