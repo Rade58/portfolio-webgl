ad51ff0e122b19c284754b43d7a79ebf2a362aaf
Rade58 <bajic.rade2@gmail.com>
2021-01-17 09:27:02 +0100

    :octocat: :space_invader:

--- some_handlers/index.ts
+++ some_handlers/index.ts
@@ -1,2 +1,10 @@
 import { appService, EE, fse, animfse } from "../state_machines/app_machine";
 import { isSSR } from "../utils/isSSR";
+
+export const setup = () => {
+  if (isSSR()) {
+    window.addEventListener("keydown", (e) => {
+      console.log(e.key);
+    });
+  }
+};
