b72afea0d26ab50df3b579ca752844923a28fc58
Rade58 <bajic.rade2@gmail.com>
2021-01-17 13:48:14 +0100

    :octocat: :space_invader:

--- content/index.ts
+++ content/index.ts
@@ -6,7 +6,7 @@ import { fse as appFse } from "../state_machines/app_machine";
 import { fse as storyFse } from "../state_machines/story_machine";
 
 // -----
-const loremipsum =
+const loremipsum: string =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros mi, lobortis vitae velit viverra, elementum efficitur diam. Suspendisse id porttitor tellus. Nulla facilisi. Praesent pulvinar maximus magna sit amet gravida. Curabitur scelerisque nibh maximus justo condimentum, non feugiat quam aliquet. Nulla aliquam turpis nec tristique laoreet. Curabitur fermentum eget nisl vel semper. Nunc ornare efficitur ante ac consequat. Praesent ornare pharetra lectus, ac commodo urna viverra sed. Vivamus in ultricies neque. Nunc at elementum justo, sit amet sollicitudin neque. Donec ornare tincidunt lectus, at aliquet enim fringilla vel. Morbi et dolor nisl. Cras bibendum eget lacus eu gravida. Integer efficitur facilisis vehicula. Morbi ut blandit arcu.";
 // ----
 
@@ -42,9 +42,9 @@ export const headingStory = (major: sketchFse): string => {
 
 // ----
 const aboutPreview = "Hello my name Is Rade I am";
-const projectsPreview = "";
-const contactPreview = "";
-const blogPreview = "";
+const projectsPreview = "Here i dome of my";
+const contactPreview = "You can contact me by";
+const blogPreview = "I wrote";
 
 // ----
 
@@ -65,18 +65,22 @@ export const storyPreview = (major: sketchFse): string => {
 
 export const storyMajorText = (
   major: sketchFse,
-  lorem: string | undefined = loremipsum
+  lorem: string | undefined
 ): string => {
+  if (typeof lorem === "string") {
+    lorem = loremipsum;
+  }
+
   if (major === sketchFse.aboutme) {
-    return lorem || `${aboutPreview} `;
+    return lorem || `${aboutPreview} `; // u nastavku template stringa dodajem dalji tekst
   }
   if (major === sketchFse.projects) {
-    return lorem || ``;
+    return lorem || `${projectsPreview} `;
   }
   if (major === sketchFse.contact) {
-    return lorem || ``;
+    return lorem || `${contactPreview} `;
   }
   if (major === sketchFse.blog) {
-    return lorem || ``;
+    return lorem || `${blogPreview} `;
   }
 };
