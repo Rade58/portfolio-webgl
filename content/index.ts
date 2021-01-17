d9ae9d234c4a8856d9a05fe293c7264ccfd595d6
Rade58 <bajic.rade2@gmail.com>
2021-01-17 10:11:06 +0100

    :octocat: :space_invader:

---
+++ content/index.ts
@@ -0,0 +1,8 @@
+import {
+  MAJOR_FINITE_STATES_ARRAY,
+  fse as sketchFse,
+} from "../sketch/middle_ground/major_states";
+import { fse as appFse } from "../state_machines/app_machine";
+import { fse as storyFse } from "../state_machines/story_machine";
+
+export {};
