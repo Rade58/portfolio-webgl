2f3af3286eab45e1a1671699c1368a8f913daf64
Rade58 <bajic.rade2@gmail.com>
2021-01-17 13:23:11 +0100

    :octocat: :space_invader:

--- content/index.ts
+++ content/index.ts
@@ -5,9 +5,20 @@ import {
 import { fse as appFse } from "../state_machines/app_machine";
 import { fse as storyFse } from "../state_machines/story_machine";
 
-export const centralMajor = (major: sketchFse) => {
+// -----
+const loremipsum =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros mi, lobortis vitae velit viverra, elementum efficitur diam. Suspendisse id porttitor tellus. Nulla facilisi. Praesent pulvinar maximus magna sit amet gravida. Curabitur scelerisque nibh maximus justo condimentum, non feugiat quam aliquet. Nulla aliquam turpis nec tristique laoreet. Curabitur fermentum eget nisl vel semper. Nunc ornare efficitur ante ac consequat. Praesent ornare pharetra lectus, ac commodo urna viverra sed. Vivamus in ultricies neque. Nunc at elementum justo, sit amet sollicitudin neque. Donec ornare tincidunt lectus, at aliquet enim fringilla vel. Morbi et dolor nisl. Cras bibendum eget lacus eu gravida. Integer efficitur facilisis vehicula. Morbi ut blandit arcu.";
+// ----
+
+export const centralMajor = (major: sketchFse): string => {
   if (major === sketchFse.aboutme) {
-    return "";
+    return "me";
+  }
+};
+
+export const headingStory = (major: sketchFse): string => {
+  if (major === sketchFse.aboutme) {
+    return "About";
   }
 };
 
@@ -16,14 +27,17 @@ const aboutPreview = "Hello my name Is Rade I am";
 
 // ----
 
-export const storyPreview = (major: sketchFse) => {
+export const storyPreview = (major: sketchFse): string => {
   if (major === sketchFse.aboutme) {
     return aboutPreview;
   }
 };
 
-export const storyMajor = (major: sketchFse) => {
+export const storyMajorText = (
+  major: sketchFse,
+  lorem: string | undefined = loremipsum
+): string => {
   if (major === sketchFse.aboutme) {
-    return `${aboutPreview} `;
+    return `${aboutPreview} ` || loremipsum;
   }
 };
