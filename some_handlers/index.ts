51751c769e894880cffbd6942b22f49c93e5a35e
Rade58 <bajic.rade2@gmail.com>
2021-01-17 09:59:25 +0100

    :octocat: :space_invader:

--- some_handlers/index.ts
+++ some_handlers/index.ts
@@ -1,16 +1,31 @@
 import { appService, EE, fse, animfse } from "../state_machines/app_machine";
+import {
+  storyService,
+  EE as EEstory,
+  fse as fseStory,
+} from "../state_machines/story_machine";
 import { isSSR } from "../utils/isSSR";
 
 export const setup = () => {
-  let initialPress = false;
-
   if (!isSSR()) {
     window.addEventListener("keydown", (e) => {
-      if (!initialPress) {
-        if (appService.state.value === fse.idling) {
-          console.log(e.key);
-          console.log(appService.state.value);
-          initialPress = true;
+      // console.log(storyService.state);
+
+      if (
+        appService.state.value === fse.idling &&
+        (storyService.state.value === fseStory.idle ||
+          storyService.state.value[fseStory.idle])
+      ) {
+        if (e.key === "ArrowLeft") {
+          appService.send({
+            type: EE.CLICK_BACK,
+          });
+        }
+
+        if (e.key === "ArrowRight") {
+          appService.send({
+            type: EE.CLICK_FORTH,
+          });
         }
       }
     });
