9ee88bf42fa2e8a64a5d6e7a684ecf6017dd4ab7
Rade58 <bajic.rade2@gmail.com>
2021-01-21 14:23:46 +0100

    :octocat: :space_invader:

--- components/story/ArticleStory.tsx
+++ components/story/ArticleStory.tsx
@@ -2,12 +2,19 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import { FunctionComponent } from "react";
+import { FunctionComponent, RefObject } from "react";
 import { css } from "@emotion/core";
 import styled from "@emotion/styled";
 
-const ArticleStory: FunctionComponent = ({ children }) => {
-  return <article>{children}</article>;
+interface PropsI {
+  articleReference: RefObject<HTMLElement>;
+}
+
+const ArticleStory: FunctionComponent<PropsI> = ({
+  children,
+  articleReference,
+}) => {
+  return <article ref={articleReference}>{children}</article>;
 };
 
 export default ArticleStory;
