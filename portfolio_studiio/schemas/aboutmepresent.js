cda1617508ed24fe3ca4acbfa28c200511f2d3ed
Rade58 <bajic.rade2@gmail.com>
2021-01-31 11:31:08 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/aboutmepresent.js
+++ portfolio_studiio/schemas/aboutmepresent.js
@@ -4,7 +4,17 @@ export default {
   type: "document",
 
   fields: [
-    { name: "title", type: "string" },
-    { name: "previewText", type: "string" },
+    { name: "title", title: "Title", type: "string" },
+    { name: "previewText", title: "Preview Text", type: "string" },
+    { name: "body", title: "Body", type: "blockContent" },
+    {
+      name: "myImage",
+      title: "My Image",
+      type: "image",
+      options: {
+        // MISLIM DA CE OVO OMOGUCITI TRANSFORMACIJU SLIKE U KRUG
+        hotspot: true,
+      },
+    },
   ],
 };
