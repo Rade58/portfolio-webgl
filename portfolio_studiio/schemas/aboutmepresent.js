0ba20f9f555b7a558046ce9e6d5d183d332dc274
Rade58 <bajic.rade2@gmail.com>
2021-01-31 11:18:16 +0100

    :octocat: :space_invader:

---
+++ portfolio_studiio/schemas/aboutmepresent.js
@@ -0,0 +1,10 @@
+export default {
+  name: "aboutmepresent",
+  title: "About Me",
+  type: "document",
+
+  fields: [
+    { name: "title", type: "string" },
+    { name: "previewText", type: "string" },
+  ],
+};
