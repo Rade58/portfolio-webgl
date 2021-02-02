392adb40178e4091bc7261ada38653d512baabf5
Rade58 <bajic.rade2@gmail.com>
2021-02-02 09:25:57 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/aboutmepresent.js
+++ portfolio_studiio/schemas/aboutmepresent.js
@@ -4,6 +4,7 @@ export default {
   type: "document",
 
   fields: [
+    { name: "major", title: "major", type: "string" },
     { name: "title", title: "Title", type: "string" },
     { name: "previewText", title: "Preview Text", type: "string" },
     { name: "bogati", title: "Bogati Tekst", type: "bogatiTekst" },
