9d47bc8cd323161ec255a17a716b5664f238af8b
Rade58 <bajic.rade2@gmail.com>
2021-02-06 16:41:35 +0100

    :octocat: :space_invader:

---
+++ portfolio_studiio/schemas/socialsvg.js
@@ -0,0 +1,32 @@
+export default {
+  name: "socialsvg",
+  title: "Social Media Svg",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "isEmail",
+      title: "Is Email",
+      type: "boolean",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "Url",
+      type: "url",
+    },
+    {
+      name: "socialImage",
+      title: "Social Image",
+      type: "image",
+    },
+  ],
+};
