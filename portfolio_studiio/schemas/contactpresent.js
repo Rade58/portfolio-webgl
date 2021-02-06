91f91e8a88662c79cfa991195f696ca184d4670b
Rade58 <bajic.rade2@gmail.com>
2021-02-06 18:02:26 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/contactpresent.js
+++ portfolio_studiio/schemas/contactpresent.js
@@ -7,5 +7,19 @@ export default {
     { name: "title", title: "Title", type: "string" },
     { name: "previewText", title: "Preview Text", type: "string" },
     { name: "bogati", title: "Bogati Tekst", type: "bogatiTekst" },
+    {
+      name: "socialIcons",
+      title: "Social Icons",
+      type: "array",
+
+      of: [
+        {
+          type: "reference",
+          to: {
+            type: "socialsvg",
+          },
+        },
+      ],
+    },
   ],
 };
