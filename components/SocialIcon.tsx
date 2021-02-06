ae44783dee7cccb1e97d69f2f07350ccd3fa5266
Rade58 <bajic.rade2@gmail.com>
2021-02-06 18:20:55 +0100

    :octocat: :space_invader:

---
+++ components/SocialIcon.tsx
@@ -0,0 +1,20 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { FunctionComponent } from "react";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+interface SocialIconEmailProps {
+  any: any;
+}
+interface SocialIconUsualProps {
+  any: any;
+}
+
+const SocialIcon: FunctionComponent = () => {
+  return null;
+};
+
+export default SocialIcon;
