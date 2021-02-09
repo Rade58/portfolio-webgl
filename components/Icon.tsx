61208ff1c747fc3225a52ca59e8ee01c054f3a1f
Rade58 <bajic.rade2@gmail.com>
2021-02-09 15:54:32 +0100

    :octocat: :space_invader:

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -6,7 +6,12 @@ import { FunctionComponent } from "react";
 import { css } from "@emotion/core";
 import styled from "@emotion/styled";
 
-import Image from "next/image";
+// OVO MI NE TREBA
+// import Image from "next/image";
+
+// KORISTIM react-svg
+import { ReactSVG } from "react-svg";
+//
 
 // BOLJE DA UNAPRED DEFINISEM DOZVOLJENA IMANA
 // KOJA CU KORISTITI I U SANITY-JU, PRI POHRANJIVANJU
@@ -38,25 +43,7 @@ const Icon: FunctionComponent<IconPropsI> = ({ url, color, name }) => {
         }
       `}
     >
-      <svg
-        tabIndex={-1}
-        /* NO NEED FOR px ON width AND height */
-        style={{ visibility: "hidden" }}
-        width="0px"
-        height="0px"
-        aria-labelledby="svg_style_holder"
-        id={`svg-style-holder-${name}`}
-        role="presentation" /* or role="imge"*/
-        lang="en"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 380 210"
-      >
-        <title id="svg_style_holder">Your Title Goes here</title>
-      </svg>
-
-      <Image layout="responsive" src="/test.svg" width="auto" height="auto" />
-      <span>Tekst</span>
+      <ReactSVG src={url} />
     </div>
   );
 };
