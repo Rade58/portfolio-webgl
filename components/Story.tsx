84c665016b38eaccd4014ab9a5adea527cad485f
Rade58 <bajic.rade2@gmail.com>
2021-02-16 22:49:40 +0100

    :octocat: :space_invader:

--- components/Story.tsx
+++ components/Story.tsx
@@ -163,32 +163,31 @@ const Story: FunctionComponent<PropsStoryI> = ({ data }) => {
             state.event.type !== EEs.ARROW_UP_TRANS
           ) { */
           if (substate === fseS.non_visible) {
+            TweenMax.fromTo(
+              articleRefs[major].current,
+              {
+                duration: 0.3,
+                ease: Power4.easeOut,
+                opacity: 1,
+                translateX: "0%",
+              },
+              {
+                opacity: 0,
+                translateX: "-200%",
+              }
+            );
             TweenMax.to(storyRef.current, {
-              duration: 0.3,
+              duration: 0.4,
               ease: Power2.easeIn,
               width: "0vw",
               height: "100%",
               // delay: 0.4,
-            }).then(() => {
-              TweenMax.fromTo(
-                articleRefs[major].current,
-                {
-                  duration: 0.8,
-                  ease: Power4.easeOut,
-                  opacity: 1,
-                  translateX: "0%",
-                },
-                {
-                  opacity: 0,
-                  translateX: "-200%",
-                }
-              );
             });
           }
 
           if (substate !== fseS.non_visible) {
             TweenMax.to(storyRef.current, {
-              duration: 1.2,
+              duration: 0.8,
               ease: Power4.easeOut,
               width: "38vw",
               height: "100%",
@@ -200,7 +199,7 @@ const Story: FunctionComponent<PropsStoryI> = ({ data }) => {
               TweenMax.fromTo(
                 articleRefs[major].current,
                 {
-                  duration: 0.2,
+                  duration: 0.4,
                   ease: Bounce.easeOut,
                   opacity: 0,
                   translateX: "-200%",
