74a2956035865121152700fe8733f6a8d3d4dd0f
Rade58 <bajic.rade2@gmail.com>
2021-02-16 22:12:25 +0100

    :octocat: :space_invader:

--- components/Story.tsx
+++ components/Story.tsx
@@ -158,61 +158,61 @@ const Story: FunctionComponent<PropsStoryI> = ({ data }) => {
 
         if (!mediaBellow) {
           //
-          if (
+          /*  if (
             state.event.type !== EEs.ARROW_UP_PUSHED &&
             state.event.type !== EEs.ARROW_UP_TRANS
-          ) {
-            if (substate === fseS.non_visible) {
+          ) { */
+          if (substate === fseS.non_visible) {
+            TweenMax.fromTo(
+              articleRefs[major].current,
+              {
+                duration: 0.8,
+                ease: Power4.easeOut,
+                opacity: 1,
+                translateX: "0%",
+              },
+              {
+                opacity: 0,
+                translateX: "-200%",
+              }
+            );
+
+            TweenMax.to(storyRef.current, {
+              duration: 0.3,
+              ease: Power2.easeIn,
+              width: "0vw",
+              height: "100%",
+              // delay: 0.4,
+            });
+          }
+
+          if (substate !== fseS.non_visible) {
+            TweenMax.to(storyRef.current, {
+              duration: 1.2,
+              ease: Power4.easeOut,
+              width: "38vw",
+              height: "100%",
+              // delay: 0.4,
+            }).then(() => {
+              console.log(articleRefs[major].current);
+
               TweenMax.fromTo(
                 articleRefs[major].current,
                 {
-                  duration: 0.8,
-                  ease: Power4.easeOut,
-                  opacity: 1,
-                  translateX: "0%",
-                },
-                {
+                  duration: 0.2,
+                  ease: Bounce.easeOut,
                   opacity: 0,
                   translateX: "-200%",
+                },
+                {
+                  opacity: 1,
+                  translateX: "0%",
                 }
               );
-
-              TweenMax.to(storyRef.current, {
-                duration: 0.3,
-                ease: Power2.easeIn,
-                width: "0vw",
-                height: "100%",
-                // delay: 0.4,
-              });
-            }
-
-            if (substate !== fseS.non_visible) {
-              TweenMax.to(storyRef.current, {
-                duration: 1.2,
-                ease: Power4.easeOut,
-                width: "38vw",
-                height: "100%",
-                // delay: 0.4,
-              }).then(() => {
-                console.log(articleRefs[major].current);
-
-                TweenMax.fromTo(
-                  articleRefs[major].current,
-                  {
-                    duration: 0.2,
-                    ease: Bounce.easeOut,
-                    opacity: 0,
-                    translateX: "-200%",
-                  },
-                  {
-                    opacity: 1,
-                    translateX: "0%",
-                  }
-                );
-              });
-              // debugger;
-            }
+            });
+            // debugger;
           }
+          // }
 
           return;
         }
