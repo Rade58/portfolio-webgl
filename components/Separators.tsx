57d1c0c3de44b7f2f85d6f4826dedc94b7c99a04
Rade58 <bajic.rade2@gmail.com>
2021-02-17 15:04:08 +0100

    :octocat: :space_invader:

--- components/Separators.tsx
+++ components/Separators.tsx
@@ -12,7 +12,35 @@ interface SeparatorPropsI {
 
 const Separators: FunctionComponent<SeparatorPropsI> = ({ emoji }) => {
   return (
-    <section className="sepa">
+    <section
+      className="sepa"
+      css={css`
+        display: flex;
+
+        & div.separator {
+          margin-right: 12%;
+          margin-left: 12%;
+          margin-top: 1vh;
+          margin-bottom: 1vh;
+          height: 2px;
+
+          &.separator-1 {
+            background-image: linear-gradient(
+              90deg,
+              rgba(158, 90, 106, 1) 18%,
+              rgba(83, 139, 148, 0.16290266106442575) 80%
+            );
+          }
+          &.separator-2 {
+            background-image: linear-gradient(
+              90deg,
+              rgba(83, 139, 148, 0.16290266106442575) 18%,
+              rgba(158, 90, 106, 1) 80%
+            );
+          }
+        }
+      `}
+    >
       <div className="separator separator-1" />
       <div>{emoji}</div>
       <div className="separator separator-2" />
