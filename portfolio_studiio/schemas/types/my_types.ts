69fe1debc8c661e94260c2970614e5333cc214fb
Rade58 <bajic.rade2@gmail.com>
2021-02-19 11:09:59 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/types/my_types.ts
+++ portfolio_studiio/schemas/types/my_types.ts
@@ -1,5 +1,17 @@
+import blogpresent from "../blogpresent";
+import projectspresent from "../projectspresent";
+
 export const enum MyTypes {
   // story
   story = "story",
+  // TYPE-OVI REFERENCED BY STORY
   aboutmepresent = "aboutmepresent",
+  contactpresent = "contactpresent",
+  projectspresent = "projectspresent",
+  blogpresent = "blogpresent",
+  // TYPE-OVI KOJE REFERENCIRA aboutmepresent
+  devsvg = "devsvg",
+
+  // TYPE-OVI KOJE REFRENCIRAJU SVI   'present'   TYPE-OVI
+  iconcolor = "iconcolor",
 }
