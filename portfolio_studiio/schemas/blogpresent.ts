14b42c4f7cf2d8b96d689d09f5fa76bcc8cc2b07
Rade58 <bajic.rade2@gmail.com>
2021-02-19 14:43:32 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/blogpresent.ts
+++ portfolio_studiio/schemas/blogpresent.ts
@@ -1,18 +1,21 @@
+import T from "./types";
+import M from "./types/my_types";
+
 export default {
-  name: "blogpresent",
+  name: M.blogpresent,
   title: "Blog",
-  type: "document",
+  type: T.document,
   fields: [
-    { name: "major", title: "major", type: "string" },
-    { name: "title", title: "Title", type: "string" },
-    { name: "previewText", title: "Preview Text", type: "string" },
-    { name: "bogati", title: "Bogati Tekst", type: "bogatiTekst" },
+    { name: "major", title: "major", type: T.string },
+    { name: "title", title: "Title", type: T.string },
+    { name: "previewText", title: "Preview Text", type: T.string },
+    { name: "bogati", title: "Bogati Tekst", type: M.bogatiTekst },
     {
       name: "iconColor",
       title: "Color For Common Icons",
-      type: "reference",
+      type: T.reference,
       to: {
-        type: "iconcolor",
+        type: M.iconcolor,
       },
     },
   ],
