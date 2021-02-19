d14856925ec5cc8d8169d66a9bbc69297af39a31
Rade58 <bajic.rade2@gmail.com>
2021-02-19 10:33:31 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/types/index.ts
+++ portfolio_studiio/schemas/types/index.ts
@@ -2,6 +2,22 @@ import { MyTypes } from "./my_types";
 
 enum FieldTypes {
   array = "array",
+  block = "block",
+  boolean = "boolean",
+  date = "date",
+  datetime = "datetime",
+  document = "document",
+  file = "file",
+  geopoint = "geopoint",
+  image = "image",
+  number = "number",
+  object = "object",
+  reference = "reference",
+  slug = "slug",
+  string = "string",
+  span = "span",
+  text = "text",
+  url = "url",
 }
 
 export type MyTypesEnum = MyTypes;
