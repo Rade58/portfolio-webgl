fc1deb2260d0c1f6a77b3f0fbb2c0e160d9f74b9
Rade58 <bajic.rade2@gmail.com>
2021-02-19 11:20:41 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/types/my_types.ts
+++ portfolio_studiio/schemas/types/my_types.ts
@@ -12,10 +12,17 @@ export const enum MyTypes {
   // TYPE-OVI KOJE REFERENCIRA aboutmepresent
   devsvg = "devsvg",
 
+  // TYPE-OVI KOJE REFERENCIRA
+
   // TYPE-OVI KOJE REFRENCIRAJU   SVI   'present'   TYPE-OVI
   iconcolor = "iconcolor",
 
   // TYPE-OVI ZA KOJE NISAM PRAVIO KOLEKCIJE ALI IH KORISTIM
   // U MOJIM TYPE-OVIMA (U SUSTINUI U PITANJU JE SAMO BOGATI TEKST)
   bogatiTekst = "bogatiTekst",
+
+  // -------------------------------
+  // TYPE-OVI KOJI SU DOSLI SA INSTALACIJOM STUDIO-A
+
+  //
 }
