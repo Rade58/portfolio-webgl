4af50d1b9e337ceea5264f17c64e04724b6675b3
Rade58 <bajic.rade2@gmail.com>
2021-02-19 13:56:19 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/types/my_types.ts
+++ portfolio_studiio/schemas/types/my_types.ts
@@ -12,17 +12,25 @@ export const enum MyTypes {
   // TYPE-OVI KOJE REFERENCIRA aboutmepresent
   devsvg = "devsvg",
 
-  // TYPE-OVI KOJE REFERENCIRA
+  // TYPE-OVI KOJE REFERENCIRA contactpresent
+  mymail = "mymail",
+  socialsvg = "socialsvg",
 
+  // TYPE-OVI KOJE REFERENCIRA projectspresent
+
+  //  (OTHER document types USED AS REFERENCES)
   // TYPE-OVI KOJE REFRENCIRAJU   SVI   'present'   TYPE-OVI
+  // I TO SU TYPE-OVI ZA KOJE SAM PRAVIO KOLEKCJE
   iconcolor = "iconcolor",
 
-  // TYPE-OVI ZA KOJE NISAM PRAVIO KOLEKCIJE ALI IH KORISTIM
-  // U MOJIM TYPE-OVIMA (U SUSTINUI U PITANJU JE SAMO BOGATI TEKST)
+  // (non-document types)
+  // TYPE-OVI, ZA KOJE NISAM PRAVIO KOLEKCIJE ALI IH KORISTIM
+  // U MOJIM `present` TYPE-OVIMA (U SUSTINUI U PITANJU JE SAMO BOGATI TEKST)
   bogatiTekst = "bogatiTekst",
 
   // -------------------------------
-  // TYPE-OVI KOJI SU DOSLI SA INSTALACIJOM STUDIO-A
+  // TYPE-OVI KOJI SU DOSLI SA INSTALACIJOM STUDIO-A (A KOJE CU EVENTUALLY UKLONITI
+  // MEDJUTIM DOBRI SU DA IH POGLEDAM U CILJU EDUKACIJE)
 
   //
 }
