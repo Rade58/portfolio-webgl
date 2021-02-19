1070a049250eb50582f5bf2b7fe36bb02dd8cc6a
Rade58 <bajic.rade2@gmail.com>
2021-02-19 15:07:58 +0100

    :octocat: :space_invader:

--- portfolio_studiio/schemas/githubinfo.ts
+++ portfolio_studiio/schemas/githubinfo.ts
@@ -1,9 +1,12 @@
+import T from "./type_names";
+import M from "./type_names/my_types";
+
 export default {
-  name: "githubinfo",
+  name: M.githubinfo,
   title: "Github",
-  type: "document",
+  type: T.document,
   fields: [
-    { name: "icon", title: "Icon", type: "image" },
-    { name: "url", title: "Your Github Url", type: "url" },
+    { name: "icon", title: "Icon", type: T.image },
+    { name: "url", title: "Your Github Url", type: T.url },
   ],
 };
