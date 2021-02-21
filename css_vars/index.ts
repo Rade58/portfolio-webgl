c66fd0933c946396cf7d5c63209b2902b96e82c0
Rade58 <bajic.rade2@gmail.com>
2021-02-21 18:50:03 +0100

    :octocat: :space_invader:

--- css_vars/index.ts
+++ css_vars/index.ts
@@ -45,3 +45,16 @@ export const backgroundImage1Reverse = /* css */ `
 // NEKE ZANIMLJIVE BOJE ZA BACKGROUND
 export const backgroundColor1 = "#1b2227";
 export const backgroundColor2 = "#2a3438";
+
+// NEKI DOBRI LINEAR GRADIENTI KOJI SU VERTICAL GRADIENTI
+
+export const verticalGradient1 = /* css */ `
+  linear-gradient( 0.2deg,  rgba(252,220,115,1) -13.1%, rgba(252,194,115,1) 31.9%, rgba(241,84,84,1) 69.6% )
+`;
+export const verticalGradient2 = /* css */ `
+  linear-gradient( 97.3deg,  rgba(25,50,70,0.81) 10.7%, rgba(155,65,25,0.72) 39.5%, rgba(255,192,0,0.81) 69.7% )
+`;
+
+export const verticalGradient3 = /* css */ `
+  linear-gradient( 111.1deg,  rgba(0,40,70,1) -4.8%, rgba(255,115,115,1) 82.7%, rgba(255,175,123,1) 97.2% )
+`;
