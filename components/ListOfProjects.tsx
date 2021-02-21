beed3285a2d8bcf32df1bb4f7cd71c78b6c2f385
Rade58 <bajic.rade2@gmail.com>
2021-02-21 12:07:31 +0100

    :octocat: :space_invader:

--- components/ListOfProjects.tsx
+++ components/ListOfProjects.tsx
@@ -29,7 +29,7 @@ const ListOfProjects: FunctionComponent<ListOfProjectsPropsI> = ({
   return (
     <div
       className="list-of-projects"
-      /* css={css`
+      css={css`
         border: olive solid 1px;
         width: 100%;
         padding-top: 18px;
@@ -38,8 +38,9 @@ const ListOfProjects: FunctionComponent<ListOfProjectsPropsI> = ({
         flex-wrap: wrap;
         justify-content: space-evenly;
 
-        & > * {
-          border: crimson solid 1px;
+        & > div.project-card {
+          /* box-shadow:  ;*/
+          border: crimson solid 0px;
           flex-basis: 288px;
           flex-shrink: 0;
           flex-grow: 1;
@@ -47,134 +48,20 @@ const ListOfProjects: FunctionComponent<ListOfProjectsPropsI> = ({
           margin-top: 8px;
 
           border-radius: 8px;
-        }
-      `} */
 
-      css={css`
-        display: grid;
-        grid-template-columns: repeat(auto-fit, 100%);
-        grid-template-rows: auto;
-        justify-content: center;
-        /* ---------------------- */
-        /* ---------------------- */
-        width: 100%;
-        @media screen and (min-width: 700px) {
-          width: 88vw;
-          grid-template-columns: repeat(auto-fit, minmax(200px, 520px));
-        }
-        @media screen and (min-width: 910px) {
-          width: 78vw;
-          grid-template-columns: repeat(auto-fit, minmax(200px, 460px));
-        }
-        @media screen and (min-width: 1086px) {
-          width: 68vw;
-          grid-template-columns: repeat(auto-fit, minmax(200px, 360px));
-        }
-        margin-right: auto;
-        margin-left: auto;
-        & div.project-card {
-          background-color: linear-gradient(
-            to right,
-            rgb(63, 44, 56),
-            rgb(38, 45, 59)
-          );
-          border: olive solid 0px;
-          margin-left: 4px;
-          margin-right: 4px;
-          margin-bottom: 8px;
-          border-radius: 2px;
-          @media screen and (max-width: 680px) {
-            margin-left: 8px;
-            margin-right: 8px;
-            /* ------------------------------------------- */
-            box-shadow: 0 0px 15px -22px rgba(0, 0, 0, 0.157),
-              0 0px 16.9px -22px rgba(0, 0, 0, 0.225),
-              0 0px 18.2px -22px rgba(0, 0, 0, 0.293),
-              0 0px 30px -22px rgba(0, 0, 0, 0.45);
-            /* ------------------------------------------- */
-          }
-          display: flex;
-          flex-direction: column;
-          position: relative;
-          & div.group-icon {
-            position: absolute;
-            /* margin-left: auto; */
-            right: 4px;
-            width: 10%;
-            border: pink solid 0px;
-            margin-right: 6px;
-            margin-top: 6px;
-            & a {
-              & img {
-                height: 2rem;
-                &:hover {
-                  transform: scale3d(1.1, 1.1, 1.1);
-                }
-              }
-            }
-          }
-          & h1 {
-            width: 78%;
-            margin-top: 5px;
-            margin-left: 6px;
-            & a {
-              color: blanchedalmond;
-              text-decoration-line: none;
-              font-weight: 400;
-              &:hover {
-                text-decoration-line: underline;
-                text-decoration-color: blanchedalmond;
-              }
-              @media screen and (max-width: 680px) {
-                text-decoration-line: underline;
-              }
-            }
-          }
-          & div.separ-post {
-            height: 2px;
-            width: 80%;
-            background-color: blanchedalmond;
+          &:nth-of-type(2n) {
             background-image: linear-gradient(
               to right,
               rgb(63, 44, 56),
               rgb(38, 45, 59)
             );
-            margin-top: auto;
-            margin-right: auto;
-            margin-left: auto;
           }
-          & p.descr {
-            margin: 10px;
-            text-align: center;
-          }
-          & div.times {
-            margin-top: 8px;
-            margin-bottom: 4px;
-            margin-left: 6px;
-            margin-right: 6px;
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            border: olive solid 0px;
-            & time {
-              font-size: 0.85rem;
-              text-decoration-line: underline;
-              color: blanchedalmond;
-              @media screen and (max-width: 680px) {
-                text-decoration-line: none;
-                color: #dd8ab8;
-              }
-            }
-            & > div {
-              width: max-content;
-              font-size: 0.85rem;
-            }
-            & > div:nth-of-type(1) {
-              margin-right: auto;
-            }
-            & > div:not(:nth-of-type(1)) {
-              margin-left: auto;
-            }
+          &:nth-of-type(2n + 1) {
+            background-image: linear-gradient(
+              to right,
+              rgb(38, 45, 59),
+              rgb(63, 44, 56)
+            );
           }
         }
       `}
