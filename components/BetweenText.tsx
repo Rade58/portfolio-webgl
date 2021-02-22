516dcf50d9e7ae137eda10701006f7ac0bcf22d9
Rade58 <bajic.rade2@gmail.com>
2021-02-22 22:06:07 +0100

    :octocat: :space_invader:

---
+++ components/BetweenText.tsx
@@ -0,0 +1,13 @@
+/* eslint jsx-a11y/anchor-is-valid: 1 */
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { FunctionComponent } from "react";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const BetweenText: FunctionComponent = () => {
+  return null;
+};
+
+export default BetweenText;
