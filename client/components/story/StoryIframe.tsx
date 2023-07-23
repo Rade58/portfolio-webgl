12aecbe250bd1082ac433b5c0c9cfae82b22f3be
Rade58 <bajic.rade2@gmail.com>
2023-07-23 16:12:47 +0200

    feat(story iframe)

--- client/components/story/StoryIframe.tsx
+++ client/components/story/StoryIframe.tsx
@@ -19,7 +19,7 @@ const StoryIframe: FunctionComponent<Props> = ({ path = "https://radedev.com" })
   <div css={css`
         height: 8vh;
       `}></div>
-      {/* {!isLoaded && <div
+      {!isLoaded && <div
         css={css`
           color: crimson;
           font-size: 2rem;
@@ -28,13 +28,68 @@ const StoryIframe: FunctionComponent<Props> = ({ path = "https://radedev.com" })
           left: 42%;
         `}
       >
-        <div>
-
-        Loading...
-        </div>
+        <div 
+          css={css`
+                            & {
+                display: inline-block;
+                position: relative;
+                width: 80px;
+                height: 80px;
+              }
+              & div {
+                position: absolute;
+                top: 33px;
+                width: 13px;
+                height: 13px;
+                border-radius: 50%;
+                background: #c0b5c9;
+                animation-timing-function: cubic-bezier(0, 1, 1, 0);
+              }
+              & div:nth-child(1) {
+                left: 8px;
+                animation: lds-ellipsis1 0.6s infinite;
+              }
+              & div:nth-child(2) {
+                left: 8px;
+                animation: lds-ellipsis2 0.6s infinite;
+              }
+              & div:nth-child(3) {
+                left: 32px;
+                animation: lds-ellipsis2 0.6s infinite;
+              }
+              & div:nth-child(4) {
+                left: 56px;
+                animation: lds-ellipsis3 0.6s infinite;
+              }
+              @keyframes lds-ellipsis1 {
+                0% {
+                  transform: scale(0);
+                }
+                100% {
+                  transform: scale(1);
+                }
+              }
+              @keyframes lds-ellipsis3 {
+                0% {
+                  transform: scale(1);
+                }
+                100% {
+                  transform: scale(0);
+                }
+              }
+              @keyframes lds-ellipsis2 {
+                0% {
+                  transform: translate(0, 0);
+                }
+                100% {
+                  transform: translate(24px, 0);
+                }
+              }
+          
+          `}
         
-        
-        </div>} */}
+        ><div></div><div></div><div></div><div></div></div>
+        </div>}
       <iframe
         css={css`
           border: #363132 0px solid;
